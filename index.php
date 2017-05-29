2ade601d2426cb1e78c0b1e9a4c3f860cd713987
narasimha <mnrao.devops@gmail.com>
2017-05-29 10:39:19 +0000

    conflict resolve

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<<<<<<< HEAD
 <title>Php Test</title>
-=======
-<title> PHP Test </title>
->>>>>>> de222482257fc03dc9c682ee4e6b2e2e6d52b000
 <style>
 h1 {color:red;}
 </style>
